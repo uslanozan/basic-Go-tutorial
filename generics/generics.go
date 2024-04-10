d1729ad919dc777377dfdf5be5f107bf64354216
Coders Club <uslanozan@gmail.com>
2024-04-10 23:36:02 +0300

    generics

---
+++ generics/generics.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	var intSlice = []int{1, 2, 3}
+	fmt.Println(sumSlice[int](intSlice))
+	
+	var float32Slice = []float32{1, 2, 3}
+	fmt.Println(sumSlice[float32](float32Slice))
+	//* fmt.Println(sumSlice(float32Slice)) you can write like that, go compiler will understand
+	
+}
+
+//* this function could take int, float32 and float64 types
+func sumSlice[T int| float32|float64](slice []T) T{
+	var sum T
+	for _, v:= range slice{
+		sum+=v
+	}
+	return sum
+}
